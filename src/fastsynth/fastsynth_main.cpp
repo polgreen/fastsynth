3d65fa00176c6cf6ede4cf23d766d473a0aef683
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2018-08-04 12:15:01 +0200

    2 random program generators

--- src/fastsynth/fastsynth_main.cpp
+++ src/fastsynth/fastsynth_main.cpp
@@ -4,8 +4,10 @@
 #include <util/cmdline.h>
 
 #include "c_frontend.h"
+#include "program_generator_frontend.h"
 #include "sygus_frontend.h"
 #include "smt2_frontend.h"
+#include "iostream"
 
 #define FASTSYNTH_OPTIONS \
    "(max-program-size):" \
@@ -17,6 +19,12 @@
    "(smt)" \
    "(literals)" \
    "(enable-division)" \
+   "(generate-N-programs):" \
+   "(program-size):" \
+   "(number-of-constants):" \
+   "(seed):" \
+   "(generate-N-programs-old):" \
+   "(number-of-params):" \
 void help(std::ostream &out)
 {
   // clang-format off
@@ -76,6 +84,12 @@ int main(int argc, const char *argv[])
     return 1;
   }
 
+  if(cmdline.isset("generate-N-programs"))
+  {
+    assemble_program(cmdline);
+    return 0;
+  }
+
   if(cmdline.args.size()!=1)
   {
     std::cerr << "Usage error, file must be given\n";
@@ -86,6 +100,17 @@ int main(int argc, const char *argv[])
   if(cmdline.isset("help")||cmdline.isset("h")||cmdline.isset("?"))
     help(std::cout);
 
+  if(cmdline.isset("generate-N-programs-old"))
+  {
+    if(has_suffix(cmdline.args.back(), ".sl"))
+    {
+      std::cout<<"Generating random programs \n";
+      generate_programs(cmdline, std::stol(
+          cmdline.get_value("generate-N-programs-old")));
+      return 0;
+      }
+     else
+      std::cerr<<"Error: generate programs must be given .sl file\n";
     return 1;
   }
 

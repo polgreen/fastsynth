077d9bd1f29c947e42a39b6205c5231ea80ceb9a
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2018-08-04 12:13:13 +0200

    fastsynth help file

--- src/fastsynth/fastsynth_main.cpp
+++ src/fastsynth/fastsynth_main.cpp
@@ -17,6 +17,55 @@
    "(smt)" \
    "(literals)" \
    "(enable-division)" \
+void help(std::ostream &out)
+{
+  // clang-format off
+  out <<
+     "\n"
+     "* *                       Fastsynth                          * *\n "
+     "* *         CounterExample Guided Inductive Synthesis        * *\n ";
+   out  <<
+     "* *              Daniel Kroening, Pascal Kesseli             * *\n"
+     "* *           Elizabeth Polgreen, Cristina David             * *\n"
+     "* *      Oxford University, Computer Science Department      * *\n"
+     "* *                  kroening@kroening.com                   * *\n"
+     "* *                                                          * *\n"
+     "\n"
+     "Usage:                       Purpose:\n"
+     "\n"
+     " fastsynth [-?] [-h] [--help]      show help\n"
+     " fastsynth file.c ...              source file names\n"
+     " fastsynth file.sl ...             source file names\n"
+     "\n"
+     "Solver options:\n"
+     " --incremental                     use incremental minisat for synthesis\n" // NOLINT(*)
+     " --simplifying-solver              use incremental minisat with simplification for synthesis\n" // NOLINT(*)
+     " --smt                             use smt solver for synthesis and verification\n" // NOLINT(*)
+     " --smt                             use smt solver for synthesis and verification\n" // NOLINT(*)
+     " --local-search                    use local-search based verification\n" // NOLINT(*)
+     " --fm                              use fourier motzkin based verification\n\n" // NOLINT(*)
+     "Solver options: sygus .sl files only\n"
+     " --neural-network                  use neural network for synthesis\n" // NOLINT(*)
+     " --beam-size                       beam size to use with neural network\n" // NOLINT(*)
+     " --enumerative-solver              use enumerative solver for synthesis\n"
+
+     "\n"
+     "Instruction set options:\n"
+     " --max-program-size N              maximum size of synthesised program\n" // NOLINT(*)
+     " --literals                        add literals from spec to instruction set\n" // NOLINT(*)
+     " --no-bitwise                      don't include any bit-wise instructions in instruction set\n" // NOLINT(*)
+     " --enable-division                 add division to instruction set\n" // NOLINT(*)
+     "\n"
+     "Enumerative program generator:\n"
+     "--generate-N-programs N            generate N random programs\n"
+     "--seed N                           seed random program generator with N\n"
+     "--program-size N                   size of programs to be generated\n"
+     "--number-params N                  number of arguments accepted by the programs to be generated\n"
+     "--number-of-constants N            max number of constants per program\n\n";
+    // clang-format on
+}
+
+
 
 int main(int argc, const char *argv[])
 {
@@ -29,7 +78,14 @@ int main(int argc, const char *argv[])
 
   if(cmdline.args.size()!=1)
   {
-    std::cerr << "Usage error\n";
+    std::cerr << "Usage error, file must be given\n";
+    help(std::cerr);
+    return 1;
+  }
+
+  if(cmdline.isset("help")||cmdline.isset("h")||cmdline.isset("?"))
+    help(std::cout);
+
     return 1;
   }
 
